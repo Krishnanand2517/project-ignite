e3147e845caf2e242f6a6519e18734c77852844a
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-11-12 03:09:04 +0530

    style: landing page: Add image and responsiveness to hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,20 +2,26 @@ import { Button } from "./index";
 
 const Hero = () => {
   return (
-    <div className="font-mono">
-      <h1 className="text-7xl mb-8 font-black text-white">
-        Project
-        <br />
-        IGNITE
-      </h1>
-      <h3 className="text-2xl mb-8 font-semibold text-[#ECF0F1]">
-        Of the coders.
-        <br />
-        By the coders.
-        <br />
-        For the coders.
-      </h3>
-      <Button className="font-semibold">Start Learning!</Button>
+    <div className="font-mono flex justify-evenly">
+      <div className="flex flex-col justify-evenly">
+        <h1 className="text-7xl 2xl:text-9xl font-black text-white">
+          Project
+          <br />
+          IGNITE
+        </h1>
+        <h3 className="text-2xl 2xl:text-4xl font-semibold text-[#ECF0F1]">
+          Of the coders.
+          <br />
+          By the coders.
+          <br />
+          For the coders.
+        </h3>
+        <Button className="font-semibold">Start Learning!</Button>
+      </div>
+
+      <div className="flex justify-end">
+        <img src="/igniteCodeMd.png" alt="ignite-code" className="w-3/4" />
+      </div>
     </div>
   );
 };
