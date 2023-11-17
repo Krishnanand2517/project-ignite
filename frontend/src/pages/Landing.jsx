5af42f4d0aaad6c02d2c2837859bf310fafca785
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-11-17 22:41:32 +0530

    style: Adjust the padding on hero section of landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -3,7 +3,7 @@ import { Hero, Resource, CTA, About } from "../components";
 const Landing = () => {
   return (
     <>
-      <div className="w-full py-4 px-20 bg-gradient-to-b from-primary via-slate-800 to-secondary">
+      <div className="w-full pt-32 pb-4 px-20 bg-gradient-to-b from-primary via-slate-800 to-secondary">
         <Hero />
       </div>
       <div className="w-full pt-36 pb-10 px-20 bg-gradient-to-t from-primary via-slate-800 to-secondary">
