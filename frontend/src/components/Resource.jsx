2a52f9e5bd9ff7d8acc9c1b8988cae529cd018cf
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-11-17 22:51:55 +0530

    refactor: Moved all the resource item data to an array

--- frontend/src/components/Resource.jsx
+++ frontend/src/components/Resource.jsx
@@ -1,29 +1,37 @@
 import { ResourceItem } from "./index";
 
 const Resource = () => {
+  const resourceItems = [
+    {
+      imagePath: "/article.png",
+      buttonText: "Articles ->",
+      imageAlt: "articles",
+      paraText: "Explore insightful articles to enhance your coding knowledge.",
+    },
+    {
+      imagePath: "/dev.png",
+      buttonText: "Projects ->",
+      imageAlt: "projects",
+      paraText:
+        "Discover real-world projects to apply and reinforce your coding skills.",
+    },
+    {
+      imagePath: "/questions.png",
+      buttonText: "Questions ->",
+      imageAlt: "questions",
+      paraText:
+        "Sharpen your problem-solving skills or prepare for coding interviews.",
+    },
+  ];
+
   return (
-    <div className="font-inconsolata text-secondary flex justify-evenly">
-      <ResourceItem
-        imagePath="/article.png"
-        buttonText="Articles ->"
-        imageAlt="articles"
-        paraText="Explore insightful articles to enhance your coding knowledge."
-      />
-
-      <ResourceItem
-        imagePath="/dev.png"
-        buttonText="Projects ->"
-        imageAlt="projects"
-        paraText="Discover real-world projects to apply and reinforce your coding skills."
-      />
-
-      <ResourceItem
-        imagePath="/questions.png"
-        buttonText="Questions ->"
-        imageAlt="questions"
-        paraText="Sharpen your problem-solving skills or prepare for coding interviews."
-      />
-    </div>
+    <ul className="font-inconsolata text-secondary flex justify-evenly">
+      {resourceItems.map((item) => (
+        <li key={item.imageAlt}>
+          <ResourceItem {...item} />
+        </li>
+      ))}
+    </ul>
   );
 };
 
