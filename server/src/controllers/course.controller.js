d894df8d58e1465182d2c45b060de19d44baf48d
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-12-19 01:30:53 +0530

    feat: Add createCourse endpoint in course controller

--- server/src/controllers/course.controller.js
+++ server/src/controllers/course.controller.js
@@ -1,5 +1,8 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import { Course } from "../models/course.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const getAllCourses = asyncHandler(async (_req, res) => {
   const courses = await Course.find({})
@@ -27,7 +30,59 @@ const getCourse = asyncHandler(async (req, res) => {
 });
 
 const createCourse = asyncHandler(async (req, res) => {
-  const { courseName, courseImage, duration, category, difficulty } = req.body;
+  if (req.account.accountType !== "instructor") {
+    throw new ApiError(
+      403,
+      "Permission denied. Only instructors can add courses."
+    );
+  }
+
+  const { courseName, duration, category, difficulty } = req.body;
+  console.log("course name:", courseName); // for debugging
+
+  if (
+    [courseName, duration, category, difficulty].some(
+      (field) => field?.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  // handle image
+  const courseImageLocalPath = req.files?.courseImage[0]?.path;
+
+  if (!courseImageLocalPath) {
+    throw new ApiError(400, "Course Image file is required");
+  }
+
+  // upload image to cloudinary
+  const courseImage = await uploadOnCloudinary(courseImageLocalPath);
+
+  if (!courseImage) {
+    throw new ApiError(400, "Course Image file upload failed");
+  }
+
+  // create course object for DB
+  const instructorId = req.account._id;
+
+  const course = await Course.create({
+    courseName,
+    courseImage: courseImage.url,
+    duration,
+    category,
+    difficulty,
+    instructor: instructorId,
+  });
+
+  const createdCourse = await Course.findById(course._id);
+
+  if (!createdCourse) {
+    throw new ApiError(500, "Something went wrong while creating the course");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdCourse, "Course created successfully"));
 });
 
 export { getAllCourses, getCourse, createCourse };
