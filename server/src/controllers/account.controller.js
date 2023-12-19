5043ba97275504559465a0d3f4f3463b06a0801d
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-12-19 11:44:08 +0530

    feat: Add registerAccount endpoint to account controller

--- server/src/controllers/account.controller.js
+++ server/src/controllers/account.controller.js
@@ -5,9 +5,65 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerAccount = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Account registered!",
+  const { username, email, password, fullName, accountType } = req.body;
+  console.log("email:", email);
+
+  if (
+    [username, email, password, fullName, accountType].some(
+      (field) => field?.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  // check for existing account
+  const existingAccount = await Account.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existingAccount) {
+    throw new ApiError(409, "User with this username or email already exists");
+  }
+
+  // handle images
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  // upload to cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file upload failed");
+  }
+
+  // create account object for DB
+  const account = await Account.create({
+    username,
+    email,
+    password,
+    fullName,
+    accountType,
+    avatarImage: avatar.url,
+  });
+
+  // remove password and refresh token field from response
+  const createdAccount = await Account.findById(account._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdAccount) {
+    throw new ApiError(
+      500,
+      "Something went wrong while registering the account"
+    );
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdAccount, "User registered successfully"));
 });
 
 export { registerAccount };
