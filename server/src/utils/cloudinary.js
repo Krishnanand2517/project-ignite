1ee69a6649c05df39b38ade84b084b118f946e50
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-12-21 22:43:12 +0530

    fix: Modify upload function to delete local file after uploading finished

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -17,6 +17,7 @@ const uploadOnCloudinary = async (localFilePath) => {
 
     console.log("File is uploaded on cloudinary", response.url);
 
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath);
