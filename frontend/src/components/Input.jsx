600deb3fc6bc5d7626fb1f6bf399336cc1cce081
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-12-26 23:21:26 +0530

    feat: Make the RegisterForm a controlled form

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -2,7 +2,7 @@ const Input = ({ label, type = "text", className = "", ...props }) => {
   return (
     <input
       type={type}
-      className={`w-full py-2 2xl:py-4 px-4 2xl:px-8 rounded-md 2xl:text-2xl ${className}`}
+      className={`w-full py-2 2xl:py-4 px-4 2xl:px-8 rounded-md 2xl:text-2xl outline-orange-400 ${className}`}
       placeholder={label}
       {...props}
     />
--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,41 +1,115 @@
+import { useState } from "react";
 import { Button, Input, ImageInput } from "./index";
 import { Link } from "react-router-dom";
 
 const RegisterForm = () => {
+  const [isStudent, setIsStudent] = useState(true);
+
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [institutionName, setInstitutionName] = useState("");
+  const [institutionCourse, setInstitutionCourse] = useState("");
+  const [institutionYear, setInstitutionYear] = useState("");
+
+  const renderStudentInputFields = () => {
+    if (!isStudent) return null;
+
+    return (
+      <>
+        <div className="mt-6">
+          <Input
+            label="Name of Your Institution"
+            value={institutionName}
+            onChange={({ target }) => setInstitutionName(target.value)}
+          />
+        </div>
+        <div>
+          <Input
+            label="Course Pursuing (e.g. B.Tech, M.A)"
+            value={institutionCourse}
+            onChange={({ target }) => setInstitutionCourse(target.value)}
+          />
+        </div>
+        <div>
+          <Input
+            label="Year of Study (e.g. 3)"
+            type="number"
+            min={1}
+            max={6}
+            value={institutionYear}
+            onChange={({ target }) => setInstitutionYear(target.value)}
+          />
+        </div>
+      </>
+    );
+  };
+
   return (
     <form className="p-14 my-32 2xl:my-48 w-full max-w-lg 2xl:max-w-2xl rounded-lg flex flex-col gap-5 2xl:gap-10 font-inconsolata mx-auto bg-black bg-opacity-20 backdrop-blur-3xl">
       <h2 className="text-2xl 2xl:text-4xl font-fira font-bold text-primary text-center mb-8">
         Create New Account
       </h2>
       <div>
-        <Input label="Full Name" />
-      </div>
-      <div>
-        <Input label="E-mail" />
-      </div>
-      <div>
-        <Input label="Name of Your Institution" />
-      </div>
-      <div>
-        <Input label="Course Pursuing (e.g. B.Tech, M.Sc.)" />
-      </div>
-      <div>
         <Input
-          label="Year of Study (e.g. 3)"
-          type="number"
-          min={1}
-          max={6}
-          className="mb-6"
+          label="Full Name"
+          value={fullName}
+          onChange={({ target }) => setFullName(target.value)}
         />
       </div>
       <div>
-        <ImageInput label="Profile Picture" className="text-primary" />
+        <Input
+          label="E-mail"
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+        />
       </div>
       <div>
-        <Input label="Password" type="password" />
+        <Input
+          label="username (only lowercase)"
+          className="lowercase"
+          value={username}
+          onChange={({ target }) => setUsername(target.value.toLowerCase())}
+        />
+      </div>
+
+      <div className="text-primary flex justify-around">
+        <span className="text-lg 2xl:text-2xl">Are you an instructor?</span>
+        <input
+          type="checkbox"
+          className="w-6 2xl:w-8 accent-orange-500 rounded-md"
+          onChange={() => setIsStudent(!isStudent)}
+        />
+      </div>
+
+      {renderStudentInputFields()}
+
+      <div>
+        <ImageInput label="Profile Picture" className="text-primary mt-6" />
       </div>
       <div>
-        <Input label="Confirm Password" type="password" />
+        <Input
+          label="Password"
+          type="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <div>
+        <Input
+          label="Confirm Password"
+          type="password"
+          value={confirmPassword}
+          onChange={({ target }) => setConfirmPassword(target.value)}
+          className={`border-2 border-white ${
+            password && password === confirmPassword
+              ? "border-green-500 outline-green-500"
+              : confirmPassword && "border-red-500 outline-red-500"
+          }`}
+        />
       </div>
       <Button textSize="text-lg 2xl:text-2xl" className="font-bold my-5 py-3">
         Create New Account
